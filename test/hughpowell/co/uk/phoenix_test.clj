a5111c7ca2fc0f4f2662d172bd866882e4d9e666
Hugh Powell <hugh@hughpowell.co.uk>
2021-06-26 11:55:12 +1000

    Init project

---
+++ test/hughpowell/co/uk/phoenix_test.clj
@@ -0,0 +1,7 @@
+(ns hughpowell.co.uk.phoenix-test
+  (:require [clojure.test :refer :all]
+            [hughpowell.co.uk.phoenix :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
